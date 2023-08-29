6c895b6788b5703b2376a36e7c34a4e7bc3db20e
Eric Batiste <ericbatiste4@gmail.com>
2023-08-28 22:28:11 -0600

    Complete ifStatements CFU

--- ifStatements.js
+++ ifStatements.js
@@ -9,10 +9,12 @@
 // Experiment with manipulating the value held within numQuarters to make sure both conditions can be achieved.
 
 var numQuarters = 0;
-
-console.log("I have enough money for a gumball");
-console.log("I don't have enough money for a gumball");
-
+numQuarters = 3
+if (numQuarters >= 2) {
+    console.log("I have enough money for a gumball");
+} else {
+    console.log("I don't have enough money for a gumball");
+};
 
 // CHALLENGE 2
 // Using the variables defined below, write code that will tell you if you have the ingredients to make a pizza. 
@@ -26,6 +28,11 @@ console.log("I don't have enough money for a gumball");
 
 // Experiment with manipulating the value held within both variables to make sure all above conditions output what you expect.
 
-var cupsOfFlour = 1;
+var cupsOfFlour = 3;
 var hasSauce = true;
+if (cupsOfFlour >= 2 && hasSauce === true) {
+    console.log("I can make pizza")
+} else {
+    console.log("I cannot make pizza")
+};
 
